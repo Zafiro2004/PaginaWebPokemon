60455cce05cde6ea94781107060af907ec90ceed
Alex <adiospallicer@cifpfbmoll.eu>
2026-03-23 20:43:53 +0100

    Lista de Pokémon con su css

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ const cuadriculaListaPokemon = document.getElementById('lista-pokemon');
 
 function renderListaPokemon(listaPokemon) {
     cuadriculaListaPokemon.innerHTML = '';
-    listaPokemon.forEach(pokemon=> {
+    listaPokemon.forEach(pokemon => {
         const carta = document.createElement('div');
         carta.classList.add('carta-pokemon');
+
         const tiposHTML = pokemon.types.map(type =>
             `<span class="badge type-${type}">${type.toUpperCase()}</span>`
         ).join('');
@@ -12,7 +13,7 @@ function renderListaPokemon(listaPokemon) {
         carta.innerHTML = `
             <a href="https://www.wikidex.net/wiki/${pokemon.name}" target="_blank" class="wikidex-link" title="Ver a ${pokemon.name} en WikiDex">
                 <div class="contenedor-imagen-carta">
-                    <img src="${pokemon.image}" alt="${pokemon.name}" class="pokemon-imagen">
+                    <img src="${pokemon.image}" alt="${pokemon.name}" class="pokemon-image">
                 </div>
             </a>
             <div class="carta-info">
@@ -21,12 +22,12 @@ function renderListaPokemon(listaPokemon) {
                     <h3 class="pokemon-nombre">${pokemon.name}</h3>
                 </a>
                 <div class="pokemon-tipos">
-                    ${typesHtml}
+                    ${tiposHTML}
                 </div>
                 <button class="add-btn" data-id="${pokemon.id}">Añadir a Equipo</button>
             </div>
         `;
-        cuadriculaListaPokemon.appendChild(carta)
-    })
+        cuadriculaListaPokemon.appendChild(carta);
+    });
 }
 renderListaPokemon(pokemonData);
